0e79720daefb6f36409bfc8097c459f19f629ebc
Maxuel Vieira Tobá Junior <maxueljunior@weg.net>
2023-11-29 14:26:02 -0300

    aprendendeo conceitos de criação de componentes e inserção de atributos e funções dentro do html

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentoComponent,
   ],
   imports: [
     BrowserModule,
