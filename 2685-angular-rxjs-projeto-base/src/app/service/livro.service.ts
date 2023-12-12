75858ceaec7fb07cd28dceeac91e437dfc436917
Maxuel Vieira Tobá Junior <maxueljunior@weg.net>
2023-12-12 16:49:09 -0300

    Implementado validação de CORS

--- 2685-angular-rxjs-projeto-base/src/app/service/livro.service.ts
+++ 2685-angular-rxjs-projeto-base/src/app/service/livro.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map, tap } from 'rxjs';
 import { Item, LivrosResultado } from '../models/interfaces';
@@ -8,7 +8,8 @@ import { Item, LivrosResultado } from '../models/interfaces';
 })
 export class LivroService {
 
-  private readonly API = 'https://www.googleapis.com/books/v1/volumes';
+  // private readonly API = 'https://www.googleapis.com/books/v1/volumes';
+  private readonly API = '/login';
 
   constructor(private http: HttpClient) { }
 
@@ -16,7 +17,11 @@ export class LivroService {
     const params = new HttpParams()
       .append('q', valorDigitado);
 
-    return this.http.get<LivrosResultado>(this.API, {params});
+    // return this.http.get<LivrosResultado>(this.API, {params});
+    return this.http.post<LivrosResultado>(this.API, {
+      username: "rodrigo",
+      password: "123"
+    });
     // .pipe(
     //   map(resultado => resultado.items ?? []),
     // );
